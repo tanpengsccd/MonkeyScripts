68a2548b4669ad9ff88725a2176b7f75d8f685ed
TomyJan <TomyJan6@gmail.com>
2024-10-01 15:31:19 +0800

    feat: add support links

--- NodeSeek-Append-Style-Remove.js
+++ NodeSeek-Append-Style-Remove.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NodeSeek 附加样式移除
 // @namespace    https://www.nodeseek.com/
-// @version      0.0.1
+// @version      0.0.2
 // @description  去除 NodeSeek 的附加样式
 // @author       TomyJan
 // @match        *://www.nodeseek.com/*
@@ -9,11 +9,19 @@
 // @grant        none
 // @run-at       document-start
 // @license      MPL-2.0 License
-// @supportURL   https://www.nodeseek.com/post-unk-1
-// @homepageURL  https://www.nodeseek.com/post-unk-1
-// @downloadURL https://update.greasyfork.org/scripts/unk.user.js
-// @updateURL https://update.greasyfork.org/scripts/unk.meta.js
+// @supportURL   https://www.nodeseek.com/post-167870-1
+// @homepageURL  https://www.nodeseek.com/post-167870-1
+// @downloadURL https://update.greasyfork.org/scripts/511007/NodeSeek%20%E9%99%84%E5%8A%A0%E6%A0%B7%E5%BC%8F%E7%A7%BB%E9%99%A4.user.js
+// @updateURL https://update.greasyfork.org/scripts/511007/NodeSeek%20%E9%99%84%E5%8A%A0%E6%A0%B7%E5%BC%8F%E7%A7%BB%E9%99%A4.meta.js
 // ==/UserScript==
+
+/**
+ * 
+ * 
+ * 当前版本更新日志
+ * 0.0.2 - 2024.10.01          !!!更新前注意备份您的配置!!!
+ * - 新增 脚本支持链接
+ */
 
 (function() {
     'use strict';
